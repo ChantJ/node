c5b3fa5eb1089bea17382237bf93452429bb0cad
Shant Joulfayan <chant.joulfayan@nmxglobalsoftware.com>
2021-02-12 07:31:30 +0400

    updating

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app=express();
 
 const port=process.env.port || 3000;
 app.get('/',(req, res)=>{
-    res.send("just got started!")
+    res.send("just got started! thats right baby")
 })
 app.listen(port, ()=>{
     console.log("got in!!")
